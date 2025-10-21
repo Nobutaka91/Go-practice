2758b77cabc8896f0ea16c473d6ab3cc893aeee9
Nob <otamasan959@gmail.com>
2025-10-22 02:26:15 +0900

    slice・map

--- main.go
+++ main.go
@@ -4,66 +4,66 @@ import (
 	"fmt"
 )
 
-const secret = "abc" // 定数secretを宣言
-
-type Os int // Os型を定義
-
-const ( // iotaを使った定数宣言
-	Windows Os = iota + 1
-	MacOS
-	Linux
-)
-
-var ( // 複数の変数をまとめて宣言
-	i int
-	s string
-	b bool
-)
 
 func main() {
+	var a1 [3]int
+	var a2 = [3]int{10, 20, 30}
+	a3 := [...]int{10, 20}
+	fmt.Printf("%v %v %v\n", a1, a2, a3)
+	fmt.Printf("%v %v\n", len(a3), cap(a3))
+	fmt.Printf("%T %T\n", a2, a3) // 型表示
 
+	var s1 []int
+	s2 := []int{}
+	fmt.Printf("s1: %[1]T %[1]v %v %v\n", s1, len(s1), cap(s2))
+	fmt.Printf("s1: %[1]T %[1]v %v %v\n", s2, len(s2), cap(s2))
+	fmt.Println(s1 == nil) // varで初期化したスライスはnilとして認識される
+	fmt.Println(s2 == nil) // リテラルで初期化したスライスはnilとして認識されない
+	s1 = append(s1, 1, 2, 3)
+	fmt.Printf("s1: %[1]T %[1]v %v %v\n", s1, len(s1), cap(s1))
+	s3 := []int{4, 5, 6}
+	s1 = append(s1, s3...) // スライスを展開して追加
+	fmt.Printf("s1: %[1]T %[1]v %v %v\n", s1, len(s1), cap(s1))
 
-	i := 2
-	ui := uint16(2) // uint16型の変数uiを宣言し、2を代入
-	fmt.Println(i)
-	fmt.Printf("i: %v %T\n", i, i ) // 変数iの値と型を表示
-	fmt.Printf("i: %[1]v %[1]T ui: %[2]v %[2]T\n", i, ui) // 変数iとuiの値と型を表示
+	s5 := make([]int, 4, 6) // 長さ4、容量6のスライスを作成
+	fmt.Printf("s5: %[1]T %[1]v %v %v\n", s5, len(s5), cap(s5))
+	s6 := s5[1:3] // [0 0]
+	s6[1] = 10
+	fmt.Printf("s5: %[1]T %[1]v %v %v\n", s5, len(s5), cap(s5))
+	fmt.Printf("s6: %[1]T %[1]v %v %v\n", s6, len(s6), cap(s6))
 
-	f := 1.23456
-	s := "hello"
-	b := true
-	fmt.Printf("f: %[1]v %[1]T\n", f) // 変数fの値と型を表示
-	fmt.Printf("s: %[1]v %[1]T\n", s) // 変数sの値と型を表示
-	fmt.Printf("b: %[1]v %[1]T\n", b) // 変数bの値と型を表示
+	sc6 := make([]int, len(s5[1:3]))
+	fmt.Printf("s5 source of copy: %v %v %v\n", s5, len(s5), cap(s5))
+	fmt.Printf("sc6 dst copy before: %v %v %v\n", sc6, len(sc6), cap(sc6))	
+
+	fs6 := s5[1:3:3]
+	fmt.Printf("s5: %v %v %v\n", s5, len(s5), cap(s5))
+	fmt.Printf("fs6: %v %v %v\n", fs6, len(fs6), cap(fs6))
+	fs6[0] = 6
+	fs6[1] = 7
+	fs6 = append(fs6, 8)
+	fmt.Printf("s5: %v %v %v\n", s5, len(s5), cap(s5))
+	fmt.Printf("fs6: %v %v %v\n", fs6, len(fs6), cap(fs6))
 	
-	pi, title := 3.14, "Go"
-	fmt.Printf("pi: %v title: %v\n", pi, title) // 変数piとtitleの値を表示
 
-	x := 10
-	y := 1.23
-	z := float64(x) + y
-	fmt.Println(z) // 変数xとyの値を表示
+	// mapとは ・・・ キーと値のペアを持つデータ構造
+	var m1 map[string]int
+	m2 := map[string]int{}
+	fmt.Printf("%v %v \n", m1, m1 == nil)
+	fmt.Printf("%v %v \n", m2, m2 == nil)
+	m2["A"] = 10
+	m2["B"] = 20
+	m2["C"] = 0
+	fmt.Printf("%v %v %v\n", m2, len(m2), m2["A"])
+	delete(m2, "A")
+	fmt.Printf("%v %v %v\n", m2, len(m2), m2["A"])
+	v, ok := m2["A"]
+	fmt.Printf("%v %v\n", v, ok)
+	v, ok = m2["C"]
+	fmt.Printf("%v %v\n", v, ok)
 
-	var ui1 uint16 // uint16型の変数ui1を宣言
-	fmt.Printf("memory address of ui1: %p\n", &ui1) // 変数ui1のメモリアドレスを表示
-	var ui2 uint16 
-	fmt.Printf("memory address of ui2: %p\n", &ui2)
-	var p1 *uint16 // ポインタ型の変数p1を宣言
-	fmt.Printf("value of p1: %v\n", p1) // 変数p1の値を表示（nilになる）
-	p1 = &ui1 // 変数p1に変数ui1のアドレスを代入
-	fmt.Printf("value of p1: %v\n", p1) // 変数p1の値を表示（ui1のアドレスになる）
-
-	// shadowing
-	ok, result := true, "A"
-	fmt.Printf("memory address of result: %p\n", &result) // 変数resultのメモリアドレスを表示
-	if ok {
-		result := "B"
-		fmt.Printf("memory address of result: %p\n", &result) // ブロック内の変数resultのメモリアドレスを表示
-		println(result) // ブロック内のresultを表示（"B"）
-	} else {
-		result := "C"
-		println(result) 
+	for k, v := range m2 {
+		fmt.Printf("%v %v\n", k, v)
 	}
-	println(result) // ブロック外のresultを表示（"A"）
-
 }
+
